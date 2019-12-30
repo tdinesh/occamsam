027b6a67bcb130daf7e0bab3b996be0e12a7c91e
armon <armon@seas.upenn.edu>
2019-12-30 13:15:33 -0500

    Update to testing

--- occamsam/optim.py
+++ occamsam/optim.py
@@ -3,10 +3,15 @@ import cvxpy.atoms
 from cvxpy.atoms import norm, mixed_norm
 from cvxpy.atoms.affine.vec import vec
 
+import numpy as np
+import scipy as sp
+import scipy.sparse
+import scipy.sparse.linalg
+
 import equivalence
 from factorgraph import GaussianFactorGraph
 
-class Optim(object):
+class WeightedLeastSquares(object):
 
     def __init__(self, graph):
 
@@ -14,22 +19,79 @@ class Optim(object):
         self.graph = graph
 
     def optimize(self):
-        pass
 
-    def update(self):
-        pass
+        Am, Ap, d, sigma_d = self.graph.observation_system()
+        Bp, t, sigma_t = self.graph.odometry_system()
 
 
-class Occam(Optim):
+class LeastSquares(object):
 
     def __init__(self, graph):
-        super(Occam).__init__(graph)
+
+        assert isinstance(graph, GaussianFactorGraph), "Expected type GaussainFactorGraph for graph, got %s" % type(graph)
+        self.graph = graph
+
+        self.M = None
+        self.P = None
+        self.res_d = None
+        self.res_t = None
 
     def optimize(self):
 
         points = self.graph.points
         landmarks = self.graph.landmarks
 
+        num_points = len(points)
+        point_dim = self.graph.point_dim
+        num_landmarks = len(landmarks)
+        landmark_dim = self.graph.landmark_dim
+
+        Am, Ap, d, _ = self.graph.observation_system()
+        Bp, t, _ = self.graph.odometry_system()
+
+        A = sp.sparse.bmat([[Am, Ap], [None, Bp]])
+        b = np.concatenate((d, t))
+        x = sp.sparse.linalg.lsqr(A, b)[0]
+        m, p = x[:num_landmarks*landmark_dim], x[num_landmarks*landmark_dim:]
+
+        self.res_d = Am.dot(m) + Ap.dot(p) - d
+        self.res_t = Bp.dot(p) - t
+
+        self.M = np.reshape(m, (num_landmarks, landmark_dim))
+        self.P = np.reshape(p, (num_points, point_dim))
+
+    def update(self):
+
+        points = self.graph.points
+        landmarks = self.graph.landmarks
+
+        for i, m in enumerate(landmarks):
+            if m.position is None:
+                m.position = self.M[i, :].copy()
+            else:
+                m.position[:] = self.M[i, :].copy()
+
+        for i, p in enumerate(points):
+            if p.position is None:
+                p.position = self.P[i, :].copy()
+            else:
+                p.position[:] = self.P[i, :].copy()
+
+
+class Occam(object):
+
+    def __init__(self, graph):
+
+        assert isinstance(graph, GaussianFactorGraph), "Expected type GaussainFactorGraph for graph, got %s" % type(graph)
+        self.graph = graph
+
+    def optimize(self):
+
+        points = self.graph.points
+        landmarks = self.graph.landmarks
+
+        num_points = len(points)
+        point_dim = self.graph.point_dim
         num_landmarks = len(landmarks)
         landmark_dim = self.graph.landmark_dim
 
@@ -39,9 +101,9 @@ class Occam(Optim):
         Bp, t = self.graph.odometry_system()
 
         M = cp.Variable((num_landmarks, landmark_dim))
-        p = cp.Variable(Ap.shape[1])
+        P = cp.Variable((num_points, point_dim))
         objective = cp.Minimize(mixed_norm(W * E * M.T))
-        constraints = [norm(Am * vec(M) + Ap * p - d)]
+        constraints = [norm(Am * vec(M) + Ap * vec(P) - d)]
 
 
 
---
+++ tests/test_optim.py
@@ -0,0 +1,63 @@
+import unittest
+
+import numpy as np
+
+import optim
+import factorgraph
+from simulator import new_simulation
+
+class TestLeastSquares(unittest.TestCase):
+
+    def test_no_noise(self):
+
+        sim = new_simulation(point_dim=3, landmark_dim=1, seed=9, observation_noise=0.0, odometry_noise=0.0)
+        fg = factorgraph.GaussianFactorGraph()
+        for f in sim.factors():
+            fg.add_factor(f)
+
+        optimizer = optim.LeastSquares(fg)
+        optimizer.optimize()
+        optimizer.update()
+
+        m_hat = np.concatenate([m.position for m in fg.landmarks])
+        p_hat = np.concatenate([m.position for m in fg.points])
+
+        m, p = np.ravel(sim.landmark_positions), np.ravel(sim.point_positions)
+
+        self.assertTrue(np.allclose(m, m_hat, atol=1e-4))
+        self.assertTrue(np.allclose(p, p_hat, atol=1e-3))
+
+    def test_noise(self):
+
+        observation_noise = 0.05
+        odometry_noise = 0.06
+
+        sim = new_simulation(point_dim=3, landmark_dim=1, seed=11, observation_noise=observation_noise, odometry_noise=odometry_noise)
+        fg = factorgraph.GaussianFactorGraph()
+        for f in sim.factors():
+            fg.add_factor(f)
+
+        optimizer = optim.LeastSquares(fg)
+        optimizer.optimize()
+        optimizer.update()
+
+        m_hat = np.concatenate([m.position for m in fg.landmarks])
+        p_hat = np.concatenate([m.position for m in fg.points])
+
+        m, p = np.ravel(sim.landmark_positions), np.ravel(sim.point_positions)
+
+        mu_m_hat = np.mean(m - m_hat)
+        sigma_m_hat = np.sqrt(np.linalg.norm(m - m_hat)**2 / len(m))
+        mu_m_hat = np.mean(p - p_hat)
+        sigma_p_hat = np.sqrt(np.linalg.norm(p - p_hat)**2 / len(p))
+
+        self.assertTrue(np.linalg.norm(optimizer.res_d)**2 < len(optimizer.res_d) * 9 * observation_noise**2)
+        self.assertTrue(np.linalg.norm(optimizer.res_t)**2 < len(optimizer.res_t) * 9 * odometry_noise**2)
+
+
+        self.assertTrue(True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
